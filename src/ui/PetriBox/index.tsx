dffee0c3f5a87cf2652a8313577f3d43bbf5e751
d-nazir <darryl@ctrlup.io>
2022-02-25 08:36:00 +0100

    feat: 🎸 return on landing page on reload

--- src/ui/PetriBox/index.tsx
+++ src/ui/PetriBox/index.tsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../core/store";
 import Cell from "../Cell";
 import Egg from "../Egg";
 import Food from "../Food";
 
 export default function PetriBox() {
+  const navigate = useNavigate();
   const { cells } = useAppSelector((state) => state.cells);
   const { food } = useAppSelector((state) => state.foods);
   const { eggs } = useAppSelector((state) => state.eggs);
+
+  useEffect(() => {
+    if (food.length === 0 && cells.length === 0 && eggs.length === 0) {
+      navigate("/");
+    }
+  }, [cells.length, eggs.length, food.length, navigate]);
 
   return (
     <div className="Petri">
