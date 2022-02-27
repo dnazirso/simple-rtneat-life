503454e03a5bbfc569435c56b94f3a5dc9bf678b
d-nazir <darryl@ctrlup.io>
2022-02-27 18:32:03 +0100

    feat: 🎸 TICK dependent hatching

--- src/core/hatchSaga/index.ts
+++ src/core/hatchSaga/index.ts
@@ -1,20 +1,32 @@
-import { delay, put, spawn, take } from "redux-saga/effects";
+import { delay, put, spawn, take, takeEvery } from "redux-saga/effects";
 import { addCell } from "../cellSlice";
 import { EggProps, removeEgg } from "../eggSlice";
-
-const DELAY = 4000;
+import { TICK } from "../timerSaga";
 
 export const HATCH = "HATCH";
+const HATCH_TICK_DELAY = 10;
+
+let nbTickPerEgg: { [EggId: EggProps["id"]]: number } = {};
 
 function* hatch({ egg }: { egg: EggProps }) {
-  yield delay(DELAY + 1000 * Math.random());
+  if (!nbTickPerEgg[egg.id]) {
+    nbTickPerEgg[egg.id] = 0;
+  }
+  if (nbTickPerEgg[egg.id] < HATCH_TICK_DELAY) {
+    nbTickPerEgg[egg.id]++;
+    return;
+  }
+  yield delay(2000 * Math.random());
   yield put(removeEgg({ id: egg.id }));
   yield put(addCell({ egg }));
+  delete nbTickPerEgg[egg.id];
 }
 
 export default function* hatchSaga() {
-  while (true) {
-    const { payload } = yield take("HATCH");
-    yield spawn(hatch, payload);
-  }
+  yield takeEvery([TICK], function* () {
+    while (true) {
+      const { payload } = yield take("HATCH");
+      yield spawn(hatch, payload);
+    }
+  });
 }
